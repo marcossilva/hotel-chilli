e24698ce0e95883d82489ee7b14748138f0e921a
Marcos Leal <marcos.leal@mosaico.com.br>
2024-11-20 00:01:45 -0300

    Revert "Removing re"
    This reverts commit db50e3688b9594dc355a532cfb3b171cf59dac38.

--- .github/workflows/url_scheduler.sh
+++
@@ -1,7 +0,0 @@
-curl -L \
-  -X POST \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer 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" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/marcossilva/hotel-chilli/actions/workflows/github-actions.yml/dispatches \
-  -d '{"ref":"master"}'
